804de014a1d8107fc4d0e2214d4c46b2aedfe0ab
Ciara Picou <ciarapicou@att.net>
2020-07-25 02:41:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,12 @@
-def reverse_each_word(string)
-array = %w(string)
-modified_array = []
+# def reverse_each_word(string)
+# array = %w(string)
+# modified_array = []
   
-  array.each do |element|
-  modified_array.push(element.reverse)
-  end
-  return modified_array
-end
+#   array.each do |element|
+#   modified_array.push(element.reverse)
+#   end
+#   return modified_array
+# end
 
 def reverse_each_word(string)
 array = %w(string)
